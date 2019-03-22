470cfb107a96c96741b90c3fea0186bde5ac1e39
tweakmy <liewjoee@yahoo.com>
2019-03-22 23:49:07 +1030

    add a method to perform lease

--- client.go
+++ client.go
@@ -9,6 +9,53 @@ import (
 	"google.golang.org/grpc"
 )
 
+// NewGrant - Prepare a new grant for the enviroment to use
+func NewGrant(ctx context.Context, endpoint string, timeinsec int64,
+	kvKey string, kvVal string) (clientv3.KV, clientv3.LeaseID, clientv3.Lease, error) {
+
+	//Create New Client
+	cl, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{endpoint},
+		DialTimeout: 2 * time.Second,
+	})
+
+	if err != nil {
+		fmt.Println("Fail to connect to etcd server " + err.Error())
+		return nil, 0, nil, err
+	}
+
+	kv := clientv3.NewKV(cl)
+	lease := clientv3.NewLease(cl)
+
+	//Grant the lease with the time
+	leaseResp, err := lease.Grant(ctx, timeinsec)
+	if err != nil {
+		fmt.Println("Could not setup the lease " + err.Error())
+		return nil, 0, nil, err
+	}
+
+	//Attemp to write the key value into etcd with the lease
+	_, err = kv.Put(ctx, kvKey, kvVal, clientv3.WithLease(leaseResp.ID))
+	if err != nil {
+		fmt.Println("Could not write to store: " + err.Error())
+		return nil, 0, nil, err
+	}
+
+	return kv, leaseResp.ID, lease, nil
+
+}
+
+// RenewLease - Keep on renewing the lease
+func RenewLease(ctx context.Context, lease clientv3.Lease, leaseID clientv3.LeaseID) (err error) {
+
+	_, err = lease.KeepAliveOnce(ctx, leaseID)
+	if err != nil {
+		fmt.Println("Could not renew lease " + err.Error())
+	}
+
+	return
+}
+
 // Testing
 func FuncA() bool {
 	// expect dial time-out on ipv4 blackhole
--- client_test.go
+++ client_test.go
@@ -1,11 +1,55 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-    if !FuncA() {
-        t.Error("Wrong")
-    }
+	if !FuncA() {
+		t.Error("Wrong")
+	}
+
+}
+
+func TestGrant(t *testing.T) {
+
+	ctx := context.TODO()
+
+	kv, leaseID, lease, _ := NewGrant(ctx, "http://127.0.0.1:2378", 2, "thekey", "thevalue")
+
+	if leaseID < 1 {
+		t.Error("Wrong ID")
+	}
+
+	ttlresp := lease.TimeToLive()
+
+	err := RenewLease(ctx, lease, leaseID)
+
+	if err != nil {
+		t.Error("It should be possible to renew the lease")
+	}
+
+	resp, _ := kv.Get(ctx, "thekey")
+	val := string(resp.Kvs[0].Value)
+
+	fmt.Println(resp.Kvs[0])
+	if val != "thevalue" {
+		t.Error("Expecting 'thekey' but got " + val)
+	}
+
+	_, _, _, err = NewGrant(ctx, "http://127.0.0.1:3000", 2, "thekey", "thevalue")
+
+	if err == nil {
+		t.Error("Should error")
+	}
+
+	err2 := RenewLease(ctx, lease, leaseID)
+
+	if err2 == nil {
+		t.Error("It should error out")
+	}
 
 }
