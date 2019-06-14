0d03345053391859ffd9983086737b5ed0648bc7
tweakmy <liewjoee@yahoo.com>
2019-06-15 00:26:43 +0930

    add global flags

---
+++ flags.go
@@ -0,0 +1,33 @@
+package main
+
+// flagKubeConfig - Path to the Kubeconfig
+var flagKubeConfig *string
+
+// flagUseKubeConfig - If there is use of local kubeconfig instead of the service account
+var flagUseKubeConfig *bool
+
+// flagWatchLabels - Node Labels to be watched from k8s api server
+var flagWatchLabels *string
+
+// flaginsecureskiptlsverify -
+var flaginsecureskiptlsverify *bool
+
+// flagcacert -
+var flagcacert *string
+
+// flagcacert -
+var flagcert *string
+
+// flagcacert -
+var flagkey *string
+
+// flagEtcdRootPath - root path where coreDNS should find path
+var flagEtcdRootPath *string
+
+// flagKubeMasterDNSName - root path where coreDNS should find path
+var flagKubeMasterDNSName *string
+
+// Use for build version
+var buildtimestamp string
+
+var githash string
