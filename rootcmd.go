70f6fd8bffdfd585fb7792b584c5014956d72c18
tweakmy <liewjoee@yahoo.com>
2019-06-16 22:08:31 +0930

    add validator

--- rootcmd.go
+++ rootcmd.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 )
@@ -17,6 +17,8 @@ func init() {
 	)
 
 	// Here you will define your flags and configuration settings.
+	flagEtcdRootPath = RootCmd.PersistentFlags().StringP("rootpath", "", "", "Etcd root path to store the domain")
+	flagKubeMasterDNSName = RootCmd.PersistentFlags().StringP("domainname", "", "", "Domain name of the kubernetes master")
 	flagKubeConfig = RootCmd.PersistentFlags().StringP("kubeconfigpath", "", kubeconfig, "enter a kubeconfig path")
 	flagUseKubeConfig = RootCmd.PersistentFlags().BoolP("usekubeconfig", "u", false, "default to use service account; if set: use kubeconfig path ")
 	flagWatchLabels = RootCmd.PersistentFlags().StringP("watchlabels", "l", "node-role.kubernetes.io/master=", "watch labels for nodes to be DNS")
@@ -24,6 +26,7 @@ func init() {
 	flagcacert = RootCmd.PersistentFlags().StringP("cacerts", "", "", "verify certificates of TLS-enabled secure servers using this CA bundle for etcd")
 	flagcert = RootCmd.PersistentFlags().StringP("cert", "", "", "identify secure client using this TLS certificate file for etcd")
 	flagkey = RootCmd.PersistentFlags().StringP("key", "", "", "identify secure client using this TLS key file for etcd")
+
 }
 
 //RootCmd - Base command
@@ -33,9 +36,22 @@ var RootCmd = &cobra.Command{
 	Long:  `???`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		fmt.Println(*flagKubeConfig)
-		fmt.Println(*flagWatchLabels)
-		fmt.Println(*flagUseKubeConfig)
+		if *flagEtcdRootPath == "" {
+			glog.Errorf("--rootpath: must not be empty")
+			os.Exit(0)
+		}
+
+		if govalidator.IsDNSName(*flagKubeMasterDNSName) {
+			glog.Errorf("--domainname: should use qualified domain name")
+			os.Exit(0)
+		}
+
+		if *flagUseKubeConfig {
+			if _, err := os.Stat(*flagKubeConfig); os.IsNotExist(err) {
+				glog.Errorf("--kubeconfigpath: kubeconfig path must exist")
+				os.Exit(0)
+			}
+		}
 	},
 }
 
